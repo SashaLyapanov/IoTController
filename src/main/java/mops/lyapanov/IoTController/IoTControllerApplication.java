72deb7ac72521de18cf3e4bcbd149aea2f19e472
SashaLyapanov <lyapanov_s@mail.ru>
2024-11-15 17:53:51 +0300

    init commit

---
+++ src/main/java/mops/lyapanov/IoTController/IoTControllerApplication.java
@@ -0,0 +1,13 @@
+package mops.lyapanov.IoTController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mops/lyapanov/IoTController/IoTControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package mops.lyapanov.IoTController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
